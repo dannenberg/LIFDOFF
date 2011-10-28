4dfe097181c2e9e1e8087c084c83e15ab4a7c679
orez- <santasassassin@gmail.com>
2011-10-28 12:25:26 -0400

    Print tops after all else now. Also removed speed improvement, it just broke on larger objects.

--- examples/3DEffect/main.py
+++ examples/3DEffect/main.py
@@ -13,16 +13,23 @@ import math
 pygame.init()
 
 black = (0x00,0x00,0x00)
-shade = (0x00,0x00,0x00)
+shade = (0x11,0x11,0x11)
 white = (0xFF,0xFF,0xFF)
+clear = (0xFF,0x00,0xFF)
 
 size=(500,500)  # size of the screen
 px,py = 0,0     # player location
 
 # Position of the walls, this just makes two rows of 5
-walls = [((-50+x*150,-150*y),(-50+x*150,-100*y),(-100+x*150,-100*y),(-100+x*150,-150*y),50) for x in xrange(5) for y in xrange(-1,2,2)]
+walls = [((-50+x*150,-150*y),(-50+x*150,-100*y),(-100+x*150,-100*y),(-100+x*150,-150*y),x+1.1) for x in xrange(5) for y in xrange(-1,2,2)]
+walls.append(((-300,-300),(-300,-350),(700,-350),(700,-300),2))
+walls.append(((-300,300),(-300,350),(700,350),(700,300),2))
+walls.append(((-300,-350),(-300,350),(-350,350),(-350,-350),2))
+walls.append(((700,-350),(700,350),(750,350),(750,-350),2))
 
 screen=pygame.display.set_mode(size)
+cap = pygame.Surface(size)
+cap.set_colorkey(clear)
 
 pygame.display.set_caption("Orez Pillars")
 
@@ -40,19 +47,21 @@ movekeys = {pygame.K_w:1,\
 # Redrawing the scene is expensive: only do this when you move or something changes.
 def redraw():
     screen.fill(white)
+    cap.fill(clear)
     pygame.draw.circle(screen, black, (size[0]/2, size[1]/2), 10)   # Your character, always in the center of the screen.
     for x in walls: # Draw each wall and each wall's 'shadow'
         botsq = [(y[0]+px+size[0]/2,y[1]+py+size[1]/2) for i,y in enumerate(x) if i!=4] # The screen coordinates of the base squares, as defined in walls
-        if False not in [y[0]<0 or y[1]<0 or y[0]>size[0] or y[1]>size[1] for y in botsq]:
-            continue  # slight performance boost: if base is entirely off-screen, don't print
-        topsq = [((y[0]-size[0]/2)*x[4], (y[1]-size[1]/2)*x[4]) for y in botsq] # the projected tops of the squares:
+        #if False not in [z for z in [(y[0]<0, y[1]<0, y[0]>size[0], y[1]>size[1]) for y in botsq]]
+        #    continue
+        topsq = [((y[0]-size[0]/2)*x[4]+size[0]/2, (y[1]-size[1]/2)*x[4]+size[1]/2) for y in botsq] # the projected tops of the squares:
         # essentially we're projecting a ray from the player to the corners of the base square to a distance of the height of the walls.
         sx1 = (botsq[0],botsq[2],topsq[2],topsq[0]) # draw a 'plane' from the opposite corners of the base to the cooresponding corners of the top.
         sx2 = (botsq[1],botsq[3],topsq[3],topsq[1]) # same, except the other opposite corners.
-        pygame.draw.polygon(screen, shade, sx1)
-        pygame.draw.polygon(screen, shade, sx2)
-        pygame.draw.polygon(screen, shade, topsq)
         pygame.draw.polygon(screen, black, botsq)
+        pygame.draw.polygon(screen, black, sx1)
+        pygame.draw.polygon(screen, black, sx2)
+        pygame.draw.polygon(cap,    shade, topsq)
+    screen.blit(cap,(0,0))
 
 redraw()            # initial drawing
 while not done:     # main loop
