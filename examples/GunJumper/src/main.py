4f468fa225f56d6401f8a01b284b2e419d897780
dannenberg <dannenberg.matt@gmail.com>
2011-11-30 12:07:12 -0500

    trying to fix processor usage. this is 2/3s as slow for me what about you?

--- examples/GunJumper/src/main.py
+++ examples/GunJumper/src/main.py
@@ -1,6 +1,7 @@
+import math
 import pygame
 import random
-import math
+import time
 from screens.screen import *
 from screens.gamescreen import *
 from screens.introscreen import *
@@ -23,12 +24,19 @@ pygame.display.set_caption("LIFDOFF")
 
 done=False
 clock = pygame.time.Clock()
+old_time = pygame.time.get_ticks()
 
 mainscreen = screens["intro"]
 
 while not done:
     clock.tick(60)
-    
+   
+    new_time = pygame.time.get_ticks()
+    waited = new_time - old_time
+    old_time = new_time
+    if waited < 60:
+        time.sleep(1.0 / (60 - waited))
+ 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done=True
