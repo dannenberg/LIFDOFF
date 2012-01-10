4506dd9469662ecef0acc5754d0c973e73593b05
orez- <bshaginaw120@gmail.com>
2012-01-10 12:07:56 -0500

    I'm an awful commit message

--- conquertheseas/src/board.py
+++ conquertheseas/src/board.py
@@ -6,6 +6,8 @@ class Board:
         self._w = w
         self._h = h
         self.cells = [None for z in xrange(w*h)]    # initialize the board size
+        self.units = []
         
     def draw_board(self, destsurface):
-        pass
+        for x in self.units:
+            x.draw_sprite(destsurface)
--- conquertheseas/src/unit.py
+++ conquertheseas/src/unit.py
@@ -4,12 +4,13 @@ class Unit:
         S = 25
         self.addons = []
         self._tileset = pygame.image.load(imgsrc)
+        self._spr_src = (50,50)     # topleft of source tile
         self._size = (w, h)         # width/height
-        self._spr_size = (w*S, h*S) # 
-        self._loc = (x,y)
-        self._spr_dest = (x*S, y*S) # 
+        self._spr_size = (w*S, h*S) # width and height in pixels
+        self._loc = (x,y)           # location on the board
+        self._spr_dest = (x*S, y*S) # pixel location on the board (from top left of board)
         
     def draw_sprite(self, destsurface):
-        for x in self.addons:
+        for x in self.addons:           # drawing subpieces
             x.draw_sprite(destsurface)
-        destsurface.blit(self._tileset, self._spr_dest)
+        destsurface.blit(self._tileset, self._spr_dest, (self._spr_src, self._spr_size))
