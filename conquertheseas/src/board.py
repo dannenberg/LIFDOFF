23605d98a49d97c2cd7a25149a7ba0dc82947fb7
orez- <bshaginaw120@gmail.com>
2012-01-11 16:07:32 -0500

    Added spaces to a file but i can't pull until i push

--- conquertheseas/src/board.py
+++ conquertheseas/src/board.py
@@ -7,14 +7,14 @@ class Board:
         self._h = h
         self.cells = [[None for _ in xrange(h)] for _ in xrange(w)]    # initialize the board size
         self.units = []
-        
+    
     def draw_board(self, destsurface):
         for x in self.units:
             x.draw_sprite(destsurface)
-
+    
     def get_cell_content(self, (x, y)):
         return self.cells[x+y*self._h]
-
+    
     def place_unit(self, unit):
         for (x, y) in unit.get_cells():
             self.cells[x][y] = unit
@@ -27,11 +27,11 @@ class Board:
         self.lift_unit(unit)
         unit.update_position(loc)
         self.place_unit(unit)
-
+    
     def add_unit(self, unit):
         self.units.append(unit)
         self.place_unit(unit)
-
+    
     def remove_unit(self, unit):
         self.units.remove(unit)
         self.lift_unit(unit)
