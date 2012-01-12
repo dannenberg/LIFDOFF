c001ea1f7b829c6d982f6e86f58135fa84863360
orez- <bshaginaw120@gmail.com>
2012-01-12 15:25:29 -0500

    And the action file, too!

---
+++ conquertheseas/src/action.py
@@ -0,0 +1,8 @@
+class Action:
+    """ An action that a unit can take: usually moving or shooting """
+    MOVE = 1
+    SHOOT = 2
+    SPECIAL = 4
+    def __init__(self, action, loc=None):
+        self.action = action
+        self.loc = loc
