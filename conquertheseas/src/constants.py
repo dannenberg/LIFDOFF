ef71d37bd1677e427e04ac8fbcc30563dba4745a
dannenberg <dannenberg.matt@gmail.com>
2012-01-15 13:17:12 -0500

    forgot to add the constants file... last two commits are bad on their own

---
+++ conquertheseas/src/constants.py
@@ -0,0 +1,31 @@
+BUTTON_OFFSET = 3
+SQUARE_SIZE = 30
+BOARD_SQUARES_X = 35
+BOARD_SQUARES_Y = 11
+BOARD_WIDTH = SQUARE_SIZE * BOARD_SQUARES_X
+BOARD_HEIGHT = SQUARE_SIZE * BOARD_SQUARES_Y
+MY_BOARD_Y = 400
+MY_BOARD_X = 201
+ENEMY_BOARD_X = 201
+ENEMY_BOARD_Y = 60
+PANEL_SQUARE_SIZE = 67
+OFFENSIVE_PANEL_SQUARES_X = 3
+OFFENSIVE_PANEL_SQUARES_Y = 5
+OFFENSIVE_PANEL_WIDTH = PANEL_SQUARE_SIZE * OFFENSIVE_PANEL_SQUARES_X
+OFFENSIVE_PANEL_HEIGHT = PANEL_SQUARE_SIZE * OFFENSIVE_PANEL_SQUARES_Y
+OFFENSIVE_PANEL_X = 0
+OFFENSIVE_PANEL_Y = 60
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
+COLORS = {"black"     :(0x00, 0x00, 0x00),
+        "gridline"  :(0x00, 0x00, 0x00, 64),
+        "white"     :(0xFF, 0xFF, 0xFF),
+        "submergedt":(0xFF, 0x99, 0x66),
+        "water"     :(0x00, 0x66, 0x99),
+        "bg"        :(0x33, 0x33, 0x33),
+        "highlight" :(0xCC, 0xCC, 0x00),
+        "lines"     :(0x00, 0x00, 0x00),
+        "sky"       :(0xCC, 0xFF, 0xFF),
+        "sand"      :(0xFF, 0xCC, 0x66),
+        "attackbut" :(0xCC, 0xCC, 0xCC),
+        "shopbg"    :(0x99, 0xCC, 0xFF)}
