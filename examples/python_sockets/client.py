3a1ff77d9eb90f949b218dae4412e22aaa537e58
dannenberg <dannenberg.matt@gmail.com>
2012-02-20 10:53:35 -0600

    client/server now handles client quitting properly

--- examples/python_sockets/client.py
+++ examples/python_sockets/client.py
@@ -1,20 +1,29 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
+HOST = socket.gethostname()
 PORT = 11151
 
 s=socket.socket( )
 s.connect((HOST, PORT))
 
+done = False
+
 def handler(s):
-    while 1:
+    while not done:
         readbuffer=s.recv(1024) # get new data
         if readbuffer:
             print readbuffer
 
 text = ""
 threading.Thread(target=handler,args=(s,)).start()  # create a new thread to handle input from server
-while text!="/quit":    # input TO server
+
+while not done:    # input TO server
     text = raw_input(">")   # blocking
-    s.send(text+"\r\n")
+    if text == "/quit":
+        done = True
+    else:
+        s.send(text+"\r\n")
+
+s.shutdown(1)
+s.close()
--- examples/python_sockets/server.py
+++ examples/python_sockets/server.py
@@ -2,30 +2,37 @@ import socket
 import threading
 import Queue
 
-HOST = "127.0.0.1"
+HOST = socket.gethostname()
 PORT = 11151
 
+print HOST
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # this is just standard, don't touch
 #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # ???
 s.bind((HOST, PORT))
-s.listen(2) # n people may connect. does not appear to work?
+s.listen(5) # n people may connect. does not appear to work?
 
 players = []
 instream = Queue.Queue()    # Threadsafe queue
 
 def accept_connections(players,stream):
     while 1:
-        conn,addr = s.accept()    # blocking, waits for new connection
+        conn, addr = s.accept()    # blocking, waits for new connection
         print "Connecting "+str(addr)
         players.append((conn,addr)) # add to playerlist
         threading.Thread(target=handler,args=(conn,addr,stream)).start()    # start a thread for them to speak
 
 def handler(conn,addr,stream):
-    while 1:
+    done = False
+    while not done:
         data = conn.recv(1024)  # see what the player says
         if data:
             toR = str(addr)+":"+data    # compile the message
             stream.put(toR)             # post to the queue, for propogation later.
+        else:
+            conn.shutdown(1)
+            conn.close()
+            players.remove((conn,addr))
+            done = True
 
 # allow people to connect
 threading.Thread(target=accept_connections,args=(players,instream)).start()
