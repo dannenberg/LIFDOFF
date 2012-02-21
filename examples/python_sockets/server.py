a48c70e9ea4825318f4f2cf6d140e0ecdf8ec828
dannenberg <dannenberg.matt@gmail.com>
2012-02-21 13:35:50 -0600

    players are now dicts and are shared safely and more consistently

--- examples/python_sockets/server.py
+++ examples/python_sockets/server.py
@@ -12,16 +12,17 @@ s.bind((HOST, PORT))
 s.listen(5) # n people may connect. does not appear to work?
 
 players = []
+players_lock = threading.Lock()
 instream = Queue.Queue()    # Threadsafe queue
-player_threads = []
 
-def accept_connections(players,stream):
+def accept_connections(stream):
     while 1:
         conn, addr = s.accept()    # blocking, waits for new connection
         print "Connecting "+str(addr)
-        players.append((conn,addr)) # add to playerlist
-        player_thread = threading.Thread(target=handler,args=(conn,addr,stream)) 
-        player_threads.append(player_thread)
+        player_thread = threading.Thread(target=handler,args=(conn,addr,stream,)) 
+        players_lock.acquire()
+        players.append({"socket":(conn,addr), "thread":player_thread, "name":"player " + str(len(players)), "ready":False}) # add to playerlist
+        players_lock.release()
         player_thread.start()   # start a thread for them to speak
 
 def handler(conn,addr,stream):
@@ -34,11 +35,13 @@ def handler(conn,addr,stream):
         else:
             conn.shutdown(1)
             conn.close()
-            players.remove((conn,addr))
+            players_lock.acquire()
+            players[:] = [player for player in players if player["socket"] != (conn,addr)]
+            players_lock.release()
             done = True
 
 # allow people to connect
-connect_thread = threading.Thread(target=accept_connections,args=(players,instream))
+connect_thread = threading.Thread(target=accept_connections,args=(instream,))
 connect_thread.start()
 
 done = False
@@ -48,14 +51,17 @@ while not done:
         if not instream.empty():    # if new messages
             dat = instream.get()    # get a message
             print dat               # post it to server
-            for x in players:       # send it to all players
-                x[0].send(dat)      # Burma Shave
+            players_lock.acquire()
+            for player in players:       # send it to all players
+                player["socket"][0].send(dat)      # Burma Shave
+            players_lock.release()
     except KeyboardInterrupt:
         connect_thread._Thread__stop() # kill the thread allowing connections so thi will actually quit
         done = True                    # given all the _ this may be dangerous...
         s.close()
-        for thread in player_threads:
-            thread._Thread__stop() 
+        players_lock.acquire()
         for player in players:
-            player[0].shutdown(1)
-            player[0].close()
+            player["thread"]._Thread__stop()
+            player["socket"][0].shutdown(1)
+            player["socket"][0].close()
+        players_lock.release()
