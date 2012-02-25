73ba1c776e56df795d9c334388c4cc1a7ecdaa6b
dannenberg <dannenberg.matt@gmail.com>
2012-02-25 12:59:52 -0600

    kicking works, disconnects are not so slick; gonna move to select before pushing

--- examples/python_sockets/client.py
+++ examples/python_sockets/client.py
@@ -1,19 +1,27 @@
 import socket
+from socket import SHUT_RDWR
+import sys
 import threading
 
 HOST = socket.gethostname()
 PORT = 11151
 
 s=socket.socket( )
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 s.connect((HOST, PORT))
 
 done = False
 
 def handler(s):
+    global done
     while not done:
-        readbuffer=s.recv(1024) # get new data
-        if readbuffer:
-            print readbuffer
+        try:
+            readbuffer=s.recv(1024) # get new data
+            if readbuffer:
+                print readbuffer
+        except Exception:
+            done = True
+            s.close()
 
 text = ""
 threading.Thread(target=handler,args=(s,)).start()  # create a new thread to handle input from server
@@ -25,12 +33,14 @@ while not done:    # input TO server
             done = True
         elif text == "/ready":
             s.send("READY "+"\r\n")
+        elif text.startswith("/kick"):
+            s.send("KICK "+text[6:]+"\r\n")
         elif text.startswith("/name"):
             s.send("NAME "+text[6:]+"\r\n")
         else:
             s.send("MSG "+text+"\r\n")
-    except KeyboardInterrupt:
+    except Exception, KeyboardInterrupt:
         done = True
-
-s.shutdown(1)
-s.close()
+        s.shutdown(SHUT_RDWR)
+        s.close()
+        sys.exit(0)
