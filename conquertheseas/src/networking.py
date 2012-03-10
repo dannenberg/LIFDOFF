1df02f479640ee3b0172035878d99772dbd453ce
dannenberg <dannenberg.matt@gmail.com>
2012-03-10 17:59:28 -0500

    told ya

--- conquertheseas/src/networking.py
+++ conquertheseas/src/networking.py
@@ -216,7 +216,7 @@ class Client(threading.Thread):
                     self.sock.close()
                 else:
                     self.buf += message
-            term, -, self.buf = self.buf.partition(RS) 
+            term, _, self.buf = self.buf.partition(RS) 
             self.process_message(term)
 
     def process_message(self, message):
